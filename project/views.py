2888e09c7ac14c224a323a8cb518c9a69acf3f81
Elton <elzeng@s.sfusd.edu>
2021-02-02 22:45:46 +0000

    I added new routes in views.py and added some more html files with that use block {{}} and some that use a string and one with a list.

--- project/views.py
+++ project/views.py
@@ -5,3 +5,11 @@ from flask import render_template, url_for
 def home():
   return render_template('index.html')
 
+@app.route('/hello/<name>')
+def hello(name):
+  return render_template('hello.html',name = name)
+
+@app.route('/list')
+def listItems():
+  name = ["Throckmorton", "Ethan", "Alvin"]
+  return render_template('names.html', name = name)
