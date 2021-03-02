643e97be0fbb3149e6c7c2473482f592ba3bcb36
Elton <32944003+EltZ36@users.noreply.github.com>
2021-03-02 14:56:04 -0800

    Update models.py

--- project/models.py
+++ project/models.py
@@ -1,8 +1,12 @@
 from project import db
 
-class Student(db.model):
-  id = db.Column(db.Integer, primary_key = True, unique = True)
-  firstname = db.Column(db.String(30), unique = False, nullable = False)
-  lastname = db.Column(db.String(50), unique = False, nullable = False)
-  reg = db.Column(db.Integer, unique = False, nullable = False)
-  grade = db.Column(db.Integeer, unique = False)
+from flask_login import UserMixin
+
+class Student(db.Model, UserMixin):
+  id = db.Column(db.Integer, primary_key=True, unique=True)
+  firstname = db.Column(db.String(30), unique=False, nullable=False)
+  lastname = db.Column(db.String(50), unique=False, nullable=False)
+  reg = db.Column(db.Integer, unique=False, nullable=False)
+  grade = db.Column(db.Integer, unique=False)
+  email = db.Column(db.String(120), unique=True)
+  password = db.Column(db.String(150), unique=False)
