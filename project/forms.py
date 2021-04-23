e9918a82b16459c651d914dfa0ed6f880cc98f56
Elton <32944003+EltZ36@users.noreply.github.com>
2021-04-23 16:35:21 -0700

    Update forms.py

--- project/forms.py
+++ project/forms.py
@@ -5,8 +5,21 @@ from wtforms.validators import (DataRequired, Email, EqualTo, Length)
 from wtforms.widgets import TextArea
 
 class LoginForm(FlaskForm):
-    firstname = StringField('Email',
+    email = StringField('Email',
                         validators=[DataRequired(), Length(max=120)])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class SignUpForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Length(max=120)])
+    name = StringField('name',
+                        validators=[DataRequired(), Length(max=120)])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+class BookForm(FlaskForm):
+    book = StringField('book',
+                        validators=[DataRequired(), Length(max=160)])
+    submit = SubmitField('Submit')
